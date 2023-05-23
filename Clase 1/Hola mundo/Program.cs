a260317d73670b7a64bfb8b470c51cb5966a09f6
TomyGauna <tgauna99@gmail.com>
2023-05-23 01:00:46 -0300

    Initial commit

---
+++ Clase 1/Hola mundo/Program.cs
@@ -0,0 +1,23 @@
+﻿// Forma vieja de hacerlo (.NET 5.0)
+/*
+using System;
+
+namespace ConsoleApp2 {
+    internal class Program{
+        static void Main (string[] args){
+            Console.WriteLine("Hola, mundo");
+        }
+    }
+} 
+*/
+
+// Forma acutal de hacerlo (.NET 6.0 en adelante)
+Console.WriteLine("Hola, mundo");
+
+
+string nombre = "Tomy";
+Console.WriteLine("Hola, " + nombre);
+
+Console.WriteLine("Hola como te llamas?");
+string nombre2 = Console.ReadLine();
+Console.WriteLine("Hola, " + nombre2);
---
+++ Ejercicio 1/Ejercicio 1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Como te llamas?");
+string nombre = Console.ReadLine();
+string time = DateTime.Now.ToString("HH:mm:ss");
+Console.WriteLine("Hola, " + nombre + ". Son las " + time);
+
